3b623bfc4e4973b96009fba2220b8e85fbbec09b
Kevin Nguyen <30872809+nguye304@users.noreply.github.com>
2018-04-24 13:43:47 -0700

    Add files via upload

---
+++ Assets/Scripts/ChasingPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChasingPlayer : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target, Vector3.up);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("gameOver");
+        }
+    }
+}
